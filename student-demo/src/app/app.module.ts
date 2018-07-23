44fc1ef7d481c6be8544af9b557712188a3ea67c
duliu1990 <492062702@qq.com>
2018-07-23 19:04:09 +0800

    update

--- student-demo/src/app/app.module.ts
+++ student-demo/src/app/app.module.ts
@@ -8,19 +8,30 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
+import {Route, RouterModule, Routes} from '@angular/router';
+import { DsahboardComponent } from './dsahboard/dsahboard.component';
+import { AddStudentComponent } from './add-student/add-student.component';
 
 registerLocaleData(zh);
 
+const appRoutes: Routes = [
+  {path: '', component: DsahboardComponent},
+  {path: 'add', component: AddStudentComponent}
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DsahboardComponent,
+    AddStudentComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    NgZorroAntdModule
+    NgZorroAntdModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
---
+++ student-demo/src/app/dsahboard/dsahboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dsahboard',
+  templateUrl: './dsahboard.component.html',
+  styleUrls: ['./dsahboard.component.css']
+})
+export class DsahboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
